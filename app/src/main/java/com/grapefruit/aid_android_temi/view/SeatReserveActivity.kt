da8027358117e4a2e1605dfd1ccf6729422960b2
송현서 <s22026@gsm.hs.kr>
2023-04-27 17:20:37 +0900

    자리 예약 퍼블리싱

---
+++ app/src/main/java/com/grapefruit/aid_android_temi/view/SeatReserveActivity.kt
@@ -0,0 +1,12 @@
+package com.grapefruit.aid_android_temi.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grapefruit.aid_android_temi.R
+
+class SeatReserveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seat_reserve)
+    }
+}
